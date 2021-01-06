c2092403e9e766bb229c41af440c52a8d99b73e0
shendypratamaa <shendypratamaa@gmail.com>
2021-01-06 10:01:31 +0700

    fixed overflow humberger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
-const navNavbar = document.querySelector('.nav');
+const navNavbar = document.querySelector('.nav__navbar');
+
 
 menuBtn.addEventListener('click', function(){
-    navNavbar.classList.toggle('.nav__show')
+    navNavbar.classList.toggle('.nav__navbar--show')
 });
